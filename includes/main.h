b03f9ccb9a4e17f0d5ced655ed833b16ca3f0238
EaseNT93 <77778300+EaseNT93@users.noreply.github.com>
2021-04-06 14:53:23 +0500

    Update main.h

--- includes/main.h
+++ includes/main.h
@@ -19,17 +19,17 @@
 
 typedef struct	s_mark
 {
-	char	empty;
-	char	obstacle;
-	char	full;
+	char		empty;
+	char		obstacle;
+	char		full;
 }				t_mark;
 
 typedef struct	s_matrix
 {
 	int		width;
 	int		height;
-	t_mark	*mark;
-	char	*data;
+	t_mark		*mark;
+	char		*data;
 
 }				t_matrix;
 
@@ -41,9 +41,9 @@ typedef struct	s_matrix_int
 }				t_matrix_int;
 
 void			ft_putstr(char *str);
-int				ft_atoi(char *str);
+int			ft_atoi(char *str);
 char			*ft_strcat(char *dest, char smb);
-int				ft_isnbr(char *str);
+int			ft_isnbr(char *str);
 t_mark			*ft_create_elem(char empty, char obstacle, char full);
 t_matrix		*ft_read_file(char *file);
 t_matrix		*ft_read_console(char *buffer);
@@ -54,13 +54,13 @@ char			matrix_get(t_matrix *matrix, size_t col, size_t row);
 void			matrix_set(t_matrix *matrix, size_t col, size_t row, char smb);
 void			ft_display_matrix(t_matrix *matrix);
 
-t_matrix_int	*alloc_matrix_int(int width, int height);
+t_matrix_int		*alloc_matrix_int(int width, int height);
 void			dealloc_matrix_int(t_matrix_int *matrix);
-int				matrix_get_int(t_matrix_int *matrix, size_t col, size_t row);
+int			matrix_get_int(t_matrix_int *matrix, size_t col, size_t row);
 void			matrix_set_int(t_matrix_int *tmp, size_t i, size_t j, int nbr);
 
-t_matrix_int	*ft_biggest_square(t_matrix *tmp);
+t_matrix_int		*ft_biggest_square(t_matrix *tmp);
 void			ft_fill_matrix(t_matrix **matrix, int col, int row, int max);
-int				*ft_get_max(t_matrix_int *tmp);
+int			*ft_get_max(t_matrix_int *tmp);
 
 #endif
